745784f920dda0d9408ec3dfca01787e1a54af55
Dev Kauan <144623928+DEVKauanrodriguesguimaraes@users.noreply.github.com>
2023-11-27 22:16:02 -0300

    Add files via upload
    Alterações caso o heroe não seja guerreiro, mago, monge, ninja.

--- index.js
+++ index.js
@@ -12,26 +12,34 @@ class heroes {
         switch (this.kindDoHero) {
             case "guerreiro":
                 weaponDoHero = "espada"
+                console.log(`O ${this.kindDoHero} atacou usando ${weaponDoHero}`)
                 break;
             case "mago":
                 weaponDoHero = "magia"
+                console.log(`O ${this.kindDoHero} atacou usando ${weaponDoHero}`)
                 break;
             case "monge":
                 weaponDoHero = "artes marciais"
+                console.log(`O ${this.kindDoHero} atacou usando ${weaponDoHero}`)
                 break;
             case "ninja":
                 weaponDoHero = "shuriken"
+                console.log(`O ${this.kindDoHero} atacou usando ${weaponDoHero}`)
                 break;
+                default:
+                    console.log("Herói não foi encontrado")
+                    break;    
         }
-        console.log(`o ${this.kindDoHero} atacou usando ${weaponDoHero}`)
     }
 }
 let heroOne = new heroes("Aquiles", "25 anos", "guerreiro");
 let heroTwo = new heroes("Merlim", "70 anos", "mago");
 let heroThree = new heroes("Gandi", "80 anos", "monge");
 let heroFour = new heroes("Itachi", "26 anos", "ninja");
+let heroFive= new heroes("Pedro", "30 anos", "arquiteto");
 
 heroOne.attack()
 heroTwo.attack()
 heroThree.attack()
 heroFour.attack()
+heroFive.attack()
